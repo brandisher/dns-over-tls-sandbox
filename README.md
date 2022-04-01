3fd4556802b0f5df3c82fa3936c39d5120bbe2dd
Chad Scribner <chad@redhat.com>
2022-04-01 09:16:46 -0400

    Working state
    - Remove the CA from the upstream as it's not needed
    - Use the openshift-cli image for the openssl-client pod
    - Change the CA CN so the server cert isn't self-signed.

--- README.md
+++ README.md
@@ -1,19 +1,15 @@
 # DNS-over-TLS Sandbox
+The purpose of this repo is to simplify the process of getting DNS-over-TLS set up using the built-in images in the cluster-dns-operator.
 
 ## Setup
+_Note: You must be logged into an OpenShift cluster before running `setup.sh`_
+
 Running `./setup.sh` will...
 
 1. Create the certificates
-2. Create the Configmap that holds the Corefile, server-cert.pem, and server-key.pem values
+2. Create the Configmap that holds the Corefile, server-cert.pem, server-key.pem, and ca-cert.pem values
 3. Create the upstream service and upstream pod. This uses the Template type to ensure that everything we pass in is consistent.
 
 ## Testing
-I've been using `oc debug upstream-tls` for testing with reasonable success. At the time of writing I was able to resolve the sample www.foo.com host via the upstream.
-
-```
-sh-4.4# dig www.foo.com @172.30.97.45 -p 5353 +tcp +short
-1.2.3.4
-```
-
-## Running Notes
-* We need to investigate kdig (or some other tool) for testing DNS-over-TLS of the upstream. The testing path is likely using the CA cert along with kdig to validate the server cert in the upstream.
+1. `oc rsh openssl-client`
+2. `openssl s_client -connect upstream-tls:5353 -servername upstream-tls`
--- setup.sh
+++ setup.sh
@@ -5,17 +5,16 @@ DNSPORT=5353
 PODDNSPORT=5353
 
 # TLS
-# https://mariadb.com/docs/security/encryption/in-transit/create-self-signed-certificates-keys-openssl/
 openssl genrsa 2048 > ca-key.pem
-openssl req -new -x509 -nodes -days 365000 -key ca-key.pem -out ca-cert.pem -subj "/C=US/ST=NC/L=Raleigh/O=Global/OU=Global/CN=$UPSTREAM.$UPSTREAM.svc.cluster.local"
-openssl req -newkey rsa:2048 -nodes -days 365000 -keyout server-key.pem -out server-req.pem -subj "/C=US/ST=NC/L=Raleigh/O=Global/OU=Global/CN=$UPSTREAM.$UPSTREAM.svc.cluster.local"
-openssl x509 -req -days 365000 -set_serial 01 -in server-req.pem -out server-cert.pem -CA ca-cert.pem -CAkey ca-key.pem
-
+openssl req -new -x509 -nodes -days 365 -key ca-key.pem -out ca-cert.pem -subj "/C=US/ST=NC/L=Raleigh/O=Global/OU=Global/CN=sample-ca"
+openssl req -newkey rsa:2048 -nodes -days 365 -keyout server-key.pem -out server-req.pem -subj "/C=US/ST=NC/L=Raleigh/O=Global/OU=Global/CN=$UPSTREAM.$UPSTREAM.svc.cluster.local"
+openssl x509 -req -days 365 -set_serial 01 -in server-req.pem -out server-cert.pem -CA ca-cert.pem -CAkey ca-key.pem
 
 COREDNS_IMAGE=$(oc get co/dns -o jsonpath='{.status.versions[?(@.name=="coredns")].version}')
+OC_IMAGE=$(oc get co/dns -o jsonpath='{.status.versions[?(@.name=="openshift-cli")].version}')
 
 cat >Corefile <<EOF
-.:$PODDNSPORT {
+tls://.:$PODDNSPORT {
   hosts {
     1.2.3.4 www.foo.com
   }
@@ -23,10 +22,11 @@ cat >Corefile <<EOF
   health
   errors
   log
+  reload
 }
 EOF
 
 oc create namespace $UPSTREAM
 oc project $UPSTREAM
-oc create configmap "$UPSTREAM" --from-file="Corefile" --from-file="server-cert.pem" --from-file="server-key.pem"
-oc process -f upstream.yaml NAME=$UPSTREAM IMAGE=$COREDNS_IMAGE --local | oc apply -f -
+oc create configmap "$UPSTREAM" --from-file="Corefile" --from-file="server-cert.pem" --from-file="server-key.pem" --from-file="ca-cert.pem"
+oc process -f upstream.yaml NAME=$UPSTREAM IMAGE=$COREDNS_IMAGE CLI=$OC_IMAGE --local | oc apply -f -
